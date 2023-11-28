948f4f684098fdde2928f967ba541bf865e397bd
Miguel La Cunza <miantolalfa04@gmail.com>
2023-11-28 15:54:34 -0500

    Finalized APIs REST en api-rest-demo, with last changes

--- src/main/java/pe/com/lacunza/controller/ProductoController.java
+++ src/main/java/pe/com/lacunza/controller/ProductoController.java
@@ -1,9 +1,11 @@
 package pe.com.lacunza.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pe.com.lacunza.model.Producto;
 import pe.com.lacunza.service.ProductoService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/productos")
@@ -12,5 +14,24 @@ public class ProductoController {
     @Autowired
     private ProductoService productoService;
 
-
+    @PostMapping
+    public Producto agregarProducto(@RequestBody Producto producto) {
+        return productoService.saveProducto(producto);
+    }
+    @GetMapping
+    public List<Producto> listarProductos() {
+        return productoService.getProductos();
+    }
+    @GetMapping("/{id}")
+    public Producto buscarProducto(@PathVariable int id) {
+        return productoService.getProductoById(id);
+    }
+    @PutMapping
+    public Producto actualizarProducto(@RequestBody Producto producto) {
+        return productoService.updateProducto(producto);
+    }
+    @DeleteMapping("/{id}")
+    public String eliminarProducto(@PathVariable int id) {
+        return productoService.deleteProducto(id);
+    }
 }
--- src/main/java/pe/com/lacunza/repository/ProductoRepository.java
+++ src/main/java/pe/com/lacunza/repository/ProductoRepository.java
@@ -12,18 +12,20 @@ public class ProductoRepository {
 
     private List<Producto> productos = new ArrayList<>();
 
+    public ProductoRepository() {
+        createProducts();
+    }
+
     // genera/crea los productos desde "la BD" simulada
     public void createProducts() {
-        productos = List.of(
-                new Producto(1, "Producto X", 15, 300),
-                new Producto(2, "Producto Y", 43, 3400),
-                new Producto(3, "Producto Z", 78, 590),
-                new Producto(4, "Producto A", 61, 770),
-                new Producto(5, "Producto B", 13, 120),
-                new Producto(6, "Producto C", 41, 990),
-                new Producto(7, "Producto D", 38, 1200),
-                new Producto(8, "Producto E", 92, 3980)
-        );
+        productos.add(new Producto(1, "Producto X", 15, 300));
+        productos.add(new Producto(2, "Producto Y", 43, 3400));
+        productos.add(new Producto(3, "Producto Z", 78, 590));
+        productos.add(new Producto(4, "Producto A", 61, 770));
+        productos.add(new Producto(5, "Producto B", 13, 120));
+        productos.add(new Producto(6, "Producto C", 41, 990));
+        productos.add(new Producto(7, "Producto D", 38, 1200));
+        productos.add(new Producto(8, "Producto E", 92, 3980));
     }
 
     // listar productos
