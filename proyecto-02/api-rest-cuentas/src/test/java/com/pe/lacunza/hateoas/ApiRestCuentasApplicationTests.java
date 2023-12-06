6d3a88209956feb375eb04eb396a3e6ac1099859
Miguel La Cunza <miantolalfa04@gmail.com>
2023-12-05 20:15:04 -0500

    Integracion to started files with 'Proyecto02'

---
+++ proyecto-02/api-rest-cuentas/src/test/java/com/pe/lacunza/hateoas/ApiRestCuentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.lacunza.hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCuentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
