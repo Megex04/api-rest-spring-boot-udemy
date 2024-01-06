c4ecd26d9b697e36dc518eb767a480ff0ec39051
Miguel La Cunza <miantolalfa04@gmail.com>
2024-01-06 16:15:24 -0500

    Add starter files to 'Sistema de encuestas'

---
+++ proyecto-03/api-encuestas/src/main/java/pe/com/lacunza/encuestas/ApiEncuestasApplication.java
@@ -0,0 +1,13 @@
+package pe.com.lacunza.encuestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEncuestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEncuestasApplication.class, args);
+	}
+
+}
---
+++ proyecto-03/api-encuestas/src/test/java/pe/com/lacunza/encuestas/ApiEncuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.lacunza.encuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEncuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
