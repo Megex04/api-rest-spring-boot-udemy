ea1ed668f39a0377aabbc8ef2a413fb789313125
Miguel La Cunza <miantolalfa04@gmail.com>
2024-03-18 17:00:55 -0500

    Add starter files to new proyect 5, Management and invoice system with emails

---
+++ proyecto-05/api-gestion-facturas/src/main/java/pe/com/lacunza/api/gestion/facturas/ApiGestionFacturasApplication.java
@@ -0,0 +1,13 @@
+package pe.com.lacunza.api.gestion.facturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionFacturasApplication.class, args);
+	}
+
+}
---
+++ proyecto-05/api-gestion-facturas/src/test/java/pe/com/lacunza/api/gestion/facturas/ApiGestionFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.lacunza.api.gestion.facturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
